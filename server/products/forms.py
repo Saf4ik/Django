647bb2cf98d0f24a9adf40871f8358868ced3350
Saf4ik <Saf4ik85@gmail.com>
2019-06-16 19:08:48 +0300

    Lesson_4 completed

---
+++ server/products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from products.models import Category
+
+
+# class CategoryForm(forms.Form):
+#     name = forms.CharField(max_length=255, required=True)
+#     description = forms.CharField(
+#         required=False,
+#         widget=forms.widgets.Textarea(attrs={'class': 'category-description'})
+#     )
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.widgets.TextInput(attrs={'class': 'name-field'}),
+            'discription': forms.widgets.Textarea(attrs={'class': 'description-field'}),
+        }
